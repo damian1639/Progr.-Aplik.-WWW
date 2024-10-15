8ecb6cd4e1ba2b9cd4d533e3212a0cc353a77297
damian1639 <115219572+damian1639@users.noreply.github.com>
2024-10-15 23:00:32 +0200

    Add files via upload

--- Filmy Oscarowe 1.1/timedate.js
+++ Filmy Oscarowe 1.1/timedate.js
@@ -24,10 +24,9 @@ function showtime() {
     var hours = now.getHours();
     var minutes = now.getMinutes();
     var seconds = now.getSeconds();
-    var timeValue = "" + ((hours > 12) ? hours - 12 : hours);
+    var timeValue = "" + ((hours < 10) ? "0" : "") + hours; 
     timeValue += ((minutes < 10) ? ":0" : ":") + minutes;
     timeValue += ((seconds < 10) ? ":0" : ":") + seconds;
-    timeValue += (hours >= 12) ? " P.M." : " A.M.";
     document.getElementById("zegarek").innerHTML = timeValue;
     timerID = setTimeout("showtime()", 1000);
     timerRunning = true;
